ac1e24bc8add0335e5d9710253657955edcc3ffb
Bredow <joe.bredow@ferguson.com>
2016-02-10 13:09:33 -0500

    clean-up

--- . helps/snippets.sql
+++ . helps/snippets.sql
@@ -18,6 +18,26 @@ WHERE ( SUBSTR ( SWD.REGION_NAME, 1 ,3 ) IN (
 				 )
 
 
+--  join to select cent business units.
+
+SUBSTR ( string, start_position [ , length ] ) [ optional ]
+
+-- BRANCH SELECT
+
+
+SELECT t.chargeId,
+       t.chargeType,
+       t.serviceMonth
+  FROM     ( SELECT chargeId,
+                    MAX ( serviceMonth ) AS serviceMonth
+               FROM invoice
+             GROUP BY chargeId ) x
+       JOIN
+           invoice t
+       ON x.chargeId = t.chargeId
+          AND x.serviceMonth = t.serviceMonth
+          
+          
 
 --salesrep_sales_fact
 SELECT DISTINCT 
